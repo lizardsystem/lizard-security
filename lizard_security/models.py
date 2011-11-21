af2bef0e1ae57db217ce796e54983c3f47d2c265
Reinout van Rees <reinout@vanrees.org>
2011-11-21 10:51:49 +0100

    Added DataSet model

--- lizard_security/models.py
+++ lizard_security/models.py
@@ -1,4 +1,23 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
-# from django.db import models
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class DataSet(models.Model):
+    """Grouping of data.
+
+    Other models can have a foreign key to DataSet to indicate they're part of
+    this data set.
+
+    """
+    name = models.CharField(_('name'),
+                            max_length=80,
+                            blank=True)
+
+    def __unicode__(self):
+        return u'<DataSet %s>' % self.name
+
+    class Meta:
+        verbose_name = _('Data set')
+        verbose_name_plural = _('Data sets')
--- lizard_security/tests.py
+++ lizard_security/tests.py
@@ -1,9 +1,20 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
+# -*- coding: utf-8 -*-
 
 from django.test import TestCase
 
+from lizard_security.models import DataSet
 
-class ExampleTest(TestCase):
 
-    def test_something(self):
-        self.assertEquals(1, 1)
+class DataSetTest(TestCase):
+
+    def test_smoke(self):
+        data_set = DataSet()
+        self.assertTrue(data_set)
+
+    def test_unicode(self):
+        non_ascii_data_set = DataSet(name='täääst')
+        non_ascii_data_set.save()
+        id = non_ascii_data_set.id
+        data_set_from_django = DataSet.objects.get(pk=id)
+        self.assertTrue(unicode(data_set_from_django))
