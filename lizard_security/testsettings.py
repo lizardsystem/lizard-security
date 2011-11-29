fb9ea6052ec819b108eeea2a3d04473c82847051
Reinout van Rees <reinout@vanrees.org>
2011-11-29 16:28:56 +0100

    Enabled our own middleware

--- lizard_security/testsettings.py
+++ lizard_security/testsettings.py
@@ -69,6 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'lizard_security.middleware.SecurityMiddleware',
     'tls.TLSRequestMiddleware',
     )
 
