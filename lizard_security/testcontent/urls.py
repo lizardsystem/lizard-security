0a96d0b343d5422be2b6e765595c6ae5c1b54688
Remco Gerlich <remco@gerlich.nl>
2014-08-05 11:39:20 +0200

    urls helpers come from different module

--- lizard_security/testcontent/urls.py
+++ lizard_security/testcontent/urls.py
@@ -1,7 +1,7 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
-from django.conf.urls.defaults import include
-from django.conf.urls.defaults import patterns
-from django.conf.urls.defaults import url
+from django.conf.urls import include
+from django.conf.urls import patterns
+from django.conf.urls import url
 from django.contrib import admin
 
 import lizard_security.urls
@@ -16,7 +16,4 @@ urlpatterns = patterns(
         name="overview"),
     (r'^admin/', include(admin.site.urls)),
     (r'^security/', include(lizard_security.urls)),
-    # url(r'^something/',
-    #     direct.import.views.some_method,
-    #     name="name_it"),
     )
